e1670fb1177894aef7aefc61b1910a8c06b999e6
Chengyu Sun <csun@calstatela.edu>
2019-06-15 10:52:48 -0700

    Add project files.

---
+++ EFCoreIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
