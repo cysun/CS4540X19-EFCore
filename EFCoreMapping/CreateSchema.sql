7163d2555add8d0975d700cdc5acf4e7a9e825e7
Chengyu Sun <csun@calstatela.edu>
2019-06-29 11:36:30 -0700

    Added EF Core Mapping examples

---
+++ EFCoreMapping/CreateSchema.sql
@@ -0,0 +1,69 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Customers] (
+    [CustomerId] int NOT NULL IDENTITY,
+    [Name] nvarchar(255) NOT NULL,
+    [Address_Street] nvarchar(max) NULL,
+    [Address_City] nvarchar(max) NULL,
+    [Address_State] nvarchar(max) NULL,
+    [Address_Zip] char(5) NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY ([CustomerId])
+);
+
+GO
+
+CREATE TABLE [Accounts] (
+    [AccountId] int NOT NULL IDENTITY,
+    [Balance] decimal(18,2) NOT NULL,
+    [DateCreated] datetime2 NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    [OwnerId] int NOT NULL,
+    [Deleted] bit NOT NULL DEFAULT 0,
+    [Discriminator] nvarchar(max) NOT NULL,
+    [Term] int NULL,
+    CONSTRAINT [PK_Accounts] PRIMARY KEY ([AccountId]),
+    CONSTRAINT [FK_Accounts_Customers_OwnerId] FOREIGN KEY ([OwnerId]) REFERENCES [Customers] ([CustomerId]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE TABLE [Phone] (
+    [CustomerId] int NOT NULL,
+    [Number] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_Phone] PRIMARY KEY ([CustomerId], [Number]),
+    CONSTRAINT [FK_Phone_Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [Customers] ([CustomerId]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE TABLE [AccountCoOwners] (
+    [AccountId] int NOT NULL,
+    [CoOwnerId] int NOT NULL,
+    CONSTRAINT [PK_AccountCoOwners] PRIMARY KEY ([AccountId], [CoOwnerId]),
+    CONSTRAINT [FK_AccountCoOwners_Accounts_AccountId] FOREIGN KEY ([AccountId]) REFERENCES [Accounts] ([AccountId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AccountCoOwners_Customers_CoOwnerId] FOREIGN KEY ([CoOwnerId]) REFERENCES [Customers] ([CustomerId]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_AccountCoOwners_CoOwnerId] ON [AccountCoOwners] ([CoOwnerId]);
+
+GO
+
+CREATE INDEX [IX_Accounts_OwnerId] ON [Accounts] ([OwnerId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20190629175449_InitialSchema', N'2.2.4-servicing-10062');
+
+GO
+
---
+++ EFCoreMapping/Data.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCoreMapping
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public Address Address { get; set; }
+
+        [InverseProperty("Owner")]
+        public List<Account> Accounts { get; set; }
+
+        public List<Phone> Phones { get; set; }
+    }
+
+    [Owned]
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        [Column(TypeName = "char(5)")]
+        public string Zip { get; set; }
+    }
+
+    public class Phone
+    {
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public string Number { get; set; }
+    }
+
+    public class Account
+    {
+        public int AccountId { get; set; }
+
+        public decimal Balance { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public int OwnerId { get; set; }
+        public Customer Owner { get; set; }
+
+        public List<AccountCoOwners> CoOwners { get; set; }
+
+        public bool Deleted { get; set; } = false;
+    }
+
+    public class AccountCoOwners
+    {
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+
+        public int CoOwnerId { get; set; }
+        public Customer CoOwner { get; set; }
+    }
+
+    public class CDAccount : Account
+    {
+        public int Term { get; set; }
+    }
+
+    class AppDbContext : DbContext
+    {
+        private static readonly string ConnectionString;
+
+        static AppDbContext()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            ConnectionString = config.GetConnectionString("Default");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Phone>().HasKey(p => new { p.CustomerId, p.Number });
+            modelBuilder.Entity<Account>().Property(a => a.DateCreated).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            modelBuilder.Entity<Account>().Property(a => a.Deleted).HasDefaultValue(false);
+            modelBuilder.Entity<Account>().HasQueryFilter(a => !a.Deleted);
+            modelBuilder.Entity<AccountCoOwners>().HasKey(a => new { a.AccountId, a.CoOwnerId });
+            modelBuilder.Entity<CDAccount>().HasBaseType<Account>();
+        }
+    }
+}
